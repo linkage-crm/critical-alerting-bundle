6b3d126efaecb57fd96735c4eccfb34261e499bd
vbudai <vbudai297@gmail.com>
2025-02-17 19:55:09 +0500

    Изменено сообщение

--- src/Entity/TelegramNotification.php
+++ src/Entity/TelegramNotification.php
@@ -8,8 +8,7 @@ class TelegramNotification extends AbstractNotification
     public function __toString(): string
     {
         return "Project: $this->project_name\n\n" .
-            "<blockquote>Message: $this->message</blockquote>\n" .
-            "<blockquote>File: \n$this->file\n" .
-            "Line: $this->line</blockquote>";
+            "<blockquote expandable>Message: $this->message</blockquote>\n" .
+            "<blockquote expandable>Trace: \n$this->trace</blockquote>";
     }
 }
